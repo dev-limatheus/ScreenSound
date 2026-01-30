8e9bc44bba5e3e364bc423f5b866e9f5bd74871c
dev-limatheus <souzalima1517@gmail.com>
2026-01-30 16:47:45 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,22 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFicha()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Nome: {artista}");
+        Console.WriteLine($"Nome: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Premium.");
+        }
+    }
+}
