cfb3edc521eed488e14d756e61aa5a7f4132ceda
dev-limatheus <souzalima1517@gmail.com>
2026-03-01 19:19:17 -0300

    Refatora menus para polimorfismo e uso de dicionário
    Refatora o sistema de menus para utilizar polimorfismo, tornando o método Executar virtual na classe base Menu e sobrescrevendo-o nas classes derivadas. Substitui o switch-case por um dicionário de opções no Program.cs, facilitando a manutenção e extensibilidade do código. Adiciona a classe MenuSair e pequenas melhorias de feedback ao usuário. Remove duplicidade e simplifica a navegação entre menus.

--- ScreenSound/Menus/Menu.cs
+++ ScreenSound/Menus/Menu.cs
@@ -1,14 +1,20 @@
-﻿namespace ScreenSound.Menus
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class Menu
 {
-    internal class Menu
+    public void ExibirTituloDaOpcao(string titulo)
     {
-        public void ExibirTituloDaOpcao(string titulo)
-        {
-            int quantidadeDeLetras = titulo.Length;
-            string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
-            Console.WriteLine(asteriscos);
-            Console.WriteLine(titulo);
-            Console.WriteLine(asteriscos + "\n");
-        }
+        int quantidadeDeLetras = titulo.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine(titulo);
+        Console.WriteLine(asteriscos + "\n");
+    }
+
+    public virtual void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
     }
 }
--- ScreenSound/Menus/MenuRegistrarBanda.cs
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -4,15 +4,16 @@ namespace ScreenSound.Menus;
 
 internal class MenuRegistrarBanda : Menu
 {
-    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
-        Console.Clear();
+        base.Executar(bandasRegistradas);
         ExibirTituloDaOpcao("Registro das bandas");
         Console.Write("Digite o nome da banda que deseja registrar: ");
         string nomeDaBanda = Console.ReadLine()!;
         Banda banda = new Banda(nomeDaBanda);
         bandasRegistradas.Add(nomeDaBanda, banda);
         Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("Retornando ao Menu..");
         Thread.Sleep(4000);
         Console.Clear();
     }
